2d33ddfff6287695a44142a41ce9ddf8ca419977
t_frrolova <tatianaaleks.frolova@gmail.com>
2024-05-08 17:54:01 +0200

    feat: setup build modes, add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
+
+const isDev = process.env.NODE_ENV === 'development';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +10,15 @@ export default defineConfig({
   css: {
     modules: {
       localsConvention: 'camelCase',
+    },
+  },
+  build: {
+    sourcemap: isDev ? true : false,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src', 'components'),
     },
   },
 });
