e505624ac72563f102d32d8d85de1ad7abbcd02c
Tanya Kostromitskaya <tetian.kos@yandex.ru>
2024-05-14 16:19:17 +0200

    test: snapshot and unit tests

---
+++ src/pages/Main/Main.test.tsx
@@ -0,0 +1,32 @@
+import { fireEvent, render } from '@testing-library/react';
+import { renderWithProviders } from '../../utils/test-utils';
+import Main from './Main';
+import { ButtonLabels } from './constants';
+
+jest.mock('../../client/client', () => {
+  return null;
+});
+
+//Mock for useNavigate
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate,
+}));
+
+describe('Main component rendering', () => {
+  test('performs snapshot testing', () => {
+    const tree = renderWithProviders(<Main />);
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Header component behavior', () => {
+  test('should navigate, when clicking on a section title button', () => {
+    const { getByText } = render(<Main />);
+    const loginButton = getByText(ButtonLabels.LOGIN);
+
+    fireEvent.click(loginButton);
+    expect(mockNavigate).toHaveBeenCalledTimes(1);
+  });
+});
