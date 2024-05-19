6ea06e7f529fc08fa4082cc3cb84fa1267e7fb63
t_frrolova <tatianaaleks.frolova@gmail.com>
2024-05-19 23:39:49 +0200

    fix: fix background texture size

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { ThemeProvider } from '@mui/material/styles';
 import Routing from './routes/Routing';
 import theme from './themes/theme';
 import { Provider } from 'react-redux';
-import { CssBaseline } from '@mui/material';
+import { Box, CssBaseline } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import initStore from './store/store.ts';
 import { Project } from '@commercetools/platform-sdk';
 import { SnackbarProvider } from 'notistack';
 import styles from '@/App.module.scss';
+import texture from '/images/texture.png';
 
 function App({ project }: { project: Project }) {
   return (
@@ -16,6 +17,18 @@ function App({ project }: { project: Project }) {
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <ThemeProvider theme={theme}>
           <SnackbarProvider maxSnack={3} classes={{ containerRoot: styles.test }}>
+            <Box
+              component="img"
+              sx={{
+                position: 'absolute',
+                zIndex: -10000,
+                width: '100%',
+                height: '100%',
+                minHeight: '100vh',
+              }}
+              alt="Ficus"
+              src={texture}
+            />
             <CssBaseline />
             <Routing />
           </SnackbarProvider>
