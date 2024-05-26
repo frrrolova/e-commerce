672ad8873e60ac1058ffc0a5fa8b6bbc4285fdc9
Tanya Kostromitskaya <tetian.kos@yandex.ru>
2024-05-27 01:07:03 +0200

    fix: hide test

--- src/pages/Main/constants.ts
+++ src/pages/Main/constants.ts
@@ -3,6 +3,8 @@ export enum PageData {
   SUBTEXT = 'Step into a lush oasis of tranquility with our exquisite collection of indoor plants, where every leaf tells a story of beauty and natural elegance.',
   TITLE_PROMO = 'Promotions',
   TITLE_DISCOUNT = 'Our special offers',
+  TOP_PLANT_ID = '254d8461-038a-4b0a-a9c5-8f1a9343a6c2',
+  CATEGORY_ID = '30968be6-31ca-4b72-a1dd-294c99f73c0b',
 }
 
 export enum ButtonLabels {
--- src/services/catalogService.ts
+++ src/services/catalogService.ts
@@ -1,7 +1,7 @@
 import client from '@/client/client';
 import { Product } from '@/types';
-import { Product as ProductCT } from '@commercetools/platform-sdk';
-
+import { mapProductProjections } from '@/utils/mapProductProjections';
+import { mapProducts } from '@/utils/mapProducts';
 class CatalogService {
   async fetchProducts(): Promise<Product[]> {
     try {
@@ -11,19 +11,41 @@ class CatalogService {
         .get({ queryArgs: { limit: 25 } })
         .execute();
 
-      // Map the commercetools products to UI-product type
-      const products: Product[] = response.body.results.map((product: ProductCT) => {
-        return {
-          id: product.id,
-          name: product.masterData.current.name[`en-GB`],
-          description: product.masterData.current.description?.[`en-GB`] || 'Product has no description',
-          images: product.masterData.current.masterVariant.images,
-          prices: product.masterData.current.masterVariant.prices,
-        };
-      });
+      const products: Product[] = mapProducts(response.body.results);
       return products;
     } catch (error) {
       console.error('Error fetching products:', error);
+      throw error;
+    }
+  }
+
+  async fetchProductById(productID: string) {
+    try {
+      const response = await client.getClient().products().withId({ ID: productID }).get().execute();
+      const product: Product[] = mapProducts([response.body]);
+      return product[0];
+    } catch (error) {
+      console.error('Error!', error);
+      throw error;
+    }
+  }
+
+  async fetchProductsByCategory(categoryId: string): Promise<Product[]> {
+    try {
+      const response = await client
+        .getClient()
+        .productProjections()
+        .get({
+          queryArgs: {
+            where: [`categories(id="${categoryId}")`],
+          },
+        })
+        .execute();
+
+      const products: Product[] = mapProductProjections(response.body.results);
+      return products;
+    } catch (error) {
+      console.error('Error fetching products by category id:', error);
       throw error;
     }
   }
