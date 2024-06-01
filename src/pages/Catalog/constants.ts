9424f3e38ab3a3cf7f19dc29f63b6d356008a959
Tetiana <kostromitskayatanya@gmail.com>
2024-06-01 17:37:47 +0200

    feat: add products sorting (#46)
    Co-authored-by: Tanya Kostromitskaya <tetian.kos@yandex.ru>

--- src/pages/Catalog/constants.ts
+++ src/pages/Catalog/constants.ts
@@ -1,3 +1,5 @@
+import { FilterAttributes } from '@/types';
+
 export enum ButtonLabels {
   APPLY = 'Apply Filters',
   CLEAR = 'Clear Filters',
@@ -9,6 +11,22 @@ export enum PageData {
   DRAWER_TOP = 73,
   NO_PRODUCTS = 'There are no products matching your filters.',
   PRICE_RANGE = 'Price Range',
+  SORT_LABEL = 'Sort By',
 }
 
 export const defaultPriceRange = [0, 200];
+
+export const SortOptions: FilterAttributes[] = [
+  {
+    key: 'price-asc',
+    label: 'Price Up',
+  },
+  {
+    key: 'price-desc',
+    label: 'Price Down',
+  },
+  {
+    key: 'name-asc',
+    label: 'Name: A to Z',
+  },
+];
---
+++ src/services/constants.ts
@@ -0,0 +1,8 @@
+const sortMapping: Record<string, string> = {
+  'price-asc': 'price asc',
+  'price-desc': 'price desc',
+  'name-asc': 'name.en-GB asc',
+  'name-desc': 'name.en-GB desc',
+};
+
+export default sortMapping;
