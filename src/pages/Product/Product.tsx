3ac0e81865c582187bf0f45f996172f1f5c714c1
t_frrolova <tatianaaleks.frolova@gmail.com>
2024-06-04 00:56:18 +0200

    fix: fix product page indents

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -75,10 +75,10 @@ function Product() {
               <Box
                 sx={{
                   display: 'flex',
-                  alignItems: {
-                    md: 'center',
-                  },
-                  justifyContent: 'center',
+                  // alignItems: {
+                  //   md: 'center',
+                  // },
+                  // justifyContent: 'center',
                   gap: {
                     xs: '15px',
                     md: '40px',
@@ -98,7 +98,14 @@ function Product() {
                       xs: '7px',
                       md: '14px',
                     },
-                    padding: 3,
+                    padding: {
+                      xs: 3,
+                      md: 2,
+                    },
+                    paddingRight: {
+                      xs: 3,
+                      md: 0,
+                    },
                   }}
                 >
                   <Box
