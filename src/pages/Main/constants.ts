cc4365375c23ec1c3ddc1ed8ca8cc28ef8255bfe
Tanya Kostromitskaya <kostromitskayatanya@gmail.com>
2024-09-18 23:13:23 +0200

    fix: update product id

--- src/pages/Main/constants.ts
+++ src/pages/Main/constants.ts
@@ -5,7 +5,7 @@ export enum PageData {
   SUBTEXT = 'Step into a lush oasis of tranquility with our exquisite collection of indoor plants, where every leaf tells a story of beauty and natural elegance.',
   TITLE_PROMO = 'Promotions',
   TITLE_DISCOUNT = 'Our special offers',
-  TOP_PLANT_ID = '6652c365-e02f-4aa9-af68-7613462bb375',
+  TOP_PLANT_ID = '13060b9f-0f5d-4163-8a42-224c3e7153a2',
   CATEGORY_ID = '0603e9fb-a319-4165-85b7-048669d10e18',
   TO_CATALOG = 'To Catalog',
 }
