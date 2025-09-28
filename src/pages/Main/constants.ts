5225e542e91e01ffbbdbc85960078ba426055a5f
Tatiana Kostromitskaya <kostromitskayatanya@gmail.com>
2025-09-28 16:57:14 +0200

    fix: add new ids on Home page (#91)

--- src/pages/Main/constants.ts
+++ src/pages/Main/constants.ts
@@ -5,8 +5,8 @@ export enum PageData {
   SUBTEXT = 'Step into a lush oasis of tranquility with our exquisite collection of indoor plants, where every leaf tells a story of beauty and natural elegance.',
   TITLE_PROMO = 'Promotions',
   TITLE_DISCOUNT = 'Our special offers',
-  TOP_PLANT_ID = '64a5194f-854c-4906-be07-3d3eda79a001',
-  CATEGORY_ID = 'daf5028f-772b-4b35-a568-385a353982d5',
+  TOP_PLANT_ID = '2704c94a-e367-4351-ba4a-7938adfb520f',
+  CATEGORY_ID = '1ee608e3-1752-4a1f-94ba-9d9e352de048',
   TO_CATALOG = 'To Catalog',
 }
 
